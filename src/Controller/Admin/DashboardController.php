9b1ede6fab2942e9cc9a9b5ca7447b3628448a93
bastienbonnamant <bastien.bonnamant@gmail.com>
2021-09-07 22:46:31 +0200

    ajout de la search console

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -11,7 +11,7 @@ use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;s
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
@@ -21,7 +21,9 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('bundles/welcome.html.twig', [
+            'user' => []
+        ]);
     }
 
     public function configureDashboard(): Dashboard
